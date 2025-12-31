1b1bfcc84986c2bf1b9f15d0efa1a0c506fde9f8
gchaitanyabharath9 <gchaitanyabharath9@gmail.com>
2025-12-31 16:25:25 -0500

    chore(qa): update gitignore for test artifacts and fix playwright config

--- qa-i18n/playwright.config.ts
+++ qa-i18n/playwright.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
         },
     ],
     webServer: {
-        // command: 'npm run start',
+        command: 'npm run start',
         url: 'http://localhost:3000',
         reuseExistingServer: true,
         timeout: 120000,
